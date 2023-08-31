1ab12132a1d57286e73d2893729c0312e2546c5d
Samuel <a2jsamuel@gmail.com>
2023-08-31 23:05:23 +0300

    Clean and add to github

--- budgetr_backend/api/v1/views/__init__.py
+++ budgetr_backend/api/v1/views/__init__.py
@@ -9,10 +9,3 @@ from api.v1.views.index import *
 from api.v1.views.expenses import *
 from api.v1.views.users import *
 from api.v1.views.budgets import *
-"""
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
-"""
--- budgetr_backend/models/base_model.py
+++ budgetr_backend/models/base_model.py
@@ -24,26 +24,6 @@ class BaseModel:
     """
         Definition of Basemodel class.
     """
-    """
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, val in kwargs.items():
-                if "created_at" == key:
-                    self.created_at = datetime.strptime(kwargs["created_at"],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif "updated_at" == key:
-                    self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                elif "__class__" == key:
-                    pass
-                else:
-                    setattr(self, key, val)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-    """
     if models.storage_t == "db":
         id = Column(String(255), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
